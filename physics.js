6cdc64ef2799b0fd9a56ab6e1556048963b584b1
sphsconormurphy <8cmurphy@stpatrick.org>
2017-05-18 13:37:14 -0500

    First Draft

--- physics.js
+++ physics.js
@@ -29,16 +29,54 @@ terminal.question("What type of equation are you looking for?",function(str){
     terminal.question("What are you trying to find? (Type the word out)",function(str){
       str.toLowerCase();
       if(str == "distance"){
-        
+        terminal.printOptions();
+        terminal.question("Type your list now.", function(strcma){
+          convertToArray(strcma);
+          if(Number.isNan(strcma[2])){
+            console.log(motion.distance(strcma[1],strcma[3]));
+            end();
+          }
+          else{
+            console.log(motion.distance1(strcma[2],strcma[3],strcma[1]));
+            end();
+          }
+        });
       }
       else if(str == "acceleration"){
-
+        terminal.printOptions();
+        terminal.question("Type your list now.", function(strcma){
+          convertToArray(strcma);
+          console.log(motion.acceleration(strcma[1],strcma[3]));
+          end();
+        });
       }
       else if(str == "time"){
-
+        terminal.printOptions();
+        terminal.question("Type your list now.", function(strcma){
+          convertToArray(strcma);
+          if(Number.isNan(strcma[3])){
+            console.log(motion.time(strcma[0],strcma[1]));
+            end();
+          }
+          else{
+            console.log(motion.time1(strcma[3],strcma[2]));
+            end();
+          }
+        });
       }
       else if(str == "velocity"){
-
+        terminal.printOptions();
+        terminal.question("Type your list now.", function(strcma){
+          convertToArray(strcma);
+          if(Number.isNan(strcma[1])){
+            console.log(motion.velocity(strcma[0],strcma[3]));
+            end();
+          }
+          else{
+            console.log(motion.velocity1(strcma[3],strcma[2]));
+            end();
+          }
+        });
       }
     });
   }
@@ -49,7 +87,7 @@ terminal.question("What type of equation are you looking for?",function(str){
         terminal.printOptions();
         terminal.question("Type your list now.", function(strcma){
           convertToArray(strcma);
-           console.log(energy.kineticEnergy(strcma[1],strcma[4]));
+          console.log(energy.kineticEnergy(strcma[1],strcma[4]));
           end();
         });
       }
@@ -57,7 +95,7 @@ terminal.question("What type of equation are you looking for?",function(str){
         terminal.printOptions();
         terminal.question("Type your list now.",function(strcma){
           convertToArray(strcma);
-          return energy.mass(strcma[5],strcma[1]);
+          console.log(energy.mass(strcma[5],strcma[1]));
           end();
         });
       }
@@ -65,7 +103,7 @@ terminal.question("What type of equation are you looking for?",function(str){
         terminal.printOptions();
         terminal.question("Type your list now.",function(strcma){
           convertToArray(strcma);
-          return energy.v(strcma[5],strcma[4]);
+          console.log(energy.v(strcma[5],strcma[4]));
           end();
         });
       }
